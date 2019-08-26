a674e176a085e1c112b791408e2e7c018eed7964
artem-barabash <ivanchik2090@gmail.com>
2019-08-26 22:21:04 +0300

    first-commit

--- js/slider.js
+++ js/slider.js
@@ -5,4 +5,33 @@ $(document).ready(function(){
       		dots: false,
       		arrows: false
      	});
+
+     	$('.partners-slider').slick({
+     		autoplay:true,
+      		autoplaySpeed:2000,
+      		dots: false,
+      		arrows: false,
+      		slidesToShow: 5,
+			slidesToScroll: 5,
+			responsive: [{
+		      breakpoint: 1025,
+		      settings: {
+		        slidesToShow: 4,
+		        infinite: true
+		      }
+		    },
+		    {
+		      breakpoint: 769,
+		      settings: {
+		        slidesToShow: 3,
+		        infinite: true
+		     }
+		 	},{
+		      breakpoint: 481,
+		      settings: {
+		        slidesToShow: 2,
+		        infinite: true
+		     }
+		 	}]
+     	});
 });
